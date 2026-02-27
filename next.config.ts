ebd3f6c80723120794e45444d6bd21ed9c6345fd
RB Soluciones <dev@rbsoluciones.co>
2026-02-27 14:12:53 -0500

    Add basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/rbsoluciones-web',
   images: {
     unoptimized: true,
   },
