b449603cc7e09db444bc51d3ec436a1127d9a1fe
RB Soluciones <dev@rbsoluciones.co>
2026-03-06 08:40:58 -0500

    fix: Revertir assetPrefix - web funcional de nuevo

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
-  assetPrefix: 'https://rbsoluciones.co',
   images: {
     unoptimized: true,
   },
